33c17bc65fa9f8b7b98958e7f79b16c02baf09a4
Young-Jin Park <youngjinpark20@gmail.com>
2022-10-03 18:05:00 -0700

    Implement basic threading

--- src/main/scala/chiselverify/VM.scala
+++ src/main/scala/chiselverify/VM.scala
@@ -1,22 +1,95 @@
 package chiselverify
 
+import scala.collection.mutable.Map
+import scala.collection.mutable.HashMap
 import chisel3._
 import chiseltest._
 
-class VM(clock: Clock) {
-  def run[R](cmd: Expr[R]): R = {
-    cmd match {
-      case Cont(r, fn) => run(fn(flatRun(r)))
-      case _: Expr[_] => flatRun(cmd)
+object VM {
+  def run[R](expr: Expr[R], clock: Clock): R = {
+    val vm = new VM[R](clock)
+    vm.run(expr)
+  }
+}
+
+protected class VM[R](clock: Clock) {
+  private val threads: Map[Expr.ThreadID, Thread[Any]] = new HashMap()
+  var threadCounter = 0
+  var time = 0
+
+  def run(expr: Expr[R]): R = {
+    new Thread(expr)
+    while (!threads.isEmpty) {
+      threads.foreach {case (id, t) => {
+        t.continue() match {
+          case Some(v) => {
+            if (id == 0) return v.asInstanceOf[R]
+            threads.remove(id)
+          }
+          case None => {}
+        }
+      }}
+      clock.step(1)
+      time += 1
     }
+    ???
   }
 
-  private def flatRun[R](cmd: Expr[R]): R = {
-    cmd match {
-      case Value(r) => r
-      case Poke(signal, value) => signal.poke(value)
-      case Peek(signal) => signal.peek()
-      case Step(cycles) => this.clock.step(cycles)
+  abstract class Monitor {}
+  case class ThreadMonitor(ids: Seq[Expr.ThreadID]) extends Monitor
+  case class ClockMonitor(time: Int) extends Monitor
+
+  private class Thread[+R](start: Expr[R]) {
+    var frame = new Frame(None, start)
+    var monitor: Option[Monitor] = None
+    val id: Expr.ThreadID = threadCounter
+
+    threadCounter += 1
+    threads(id) = this
+
+    protected class Frame(val parent: Option[Frame], var expr: Expr[Any])
+
+    def continue(): Option[R] = {
+      val resolved = monitor match {
+        case Some(monitor) => monitor match {
+          case ThreadMonitor(ids) => ids.forall(id => !threads.contains(id))
+          case ClockMonitor(time) => time <= VM.this.time
+        }
+        case None => true
+      }
+      if (resolved) monitor = None
+
+      while (monitor == None) {
+        frame.expr match {
+          case Cont(expr, cn) => expr match {
+            case Cont(_,_) => frame = new Frame(Some(frame), expr)
+            case _ =>         frame.expr = cn(flat(expr))
+          }
+          case expr => {
+            frame.parent match {
+              case Some(p) => frame = p
+              case None    => return Some(flat(expr.asInstanceOf[Expr[R]]))
+            }
+            frame.expr match {
+              case Cont(_, cn) => frame.expr = cn(flat(expr))
+              case _ => ???
+            }
+          }
+        }
+      }
+      None
+    }
+
+    private def flat[R](expr: Expr[R]): R = {
+      expr match {
+        case Join(threadids) => monitor = Some(new ThreadMonitor(threadids))
+        case Step(cycles) => monitor = Some(new ClockMonitor(VM.this.time + cycles))
+        case Fork(expr) => new Thread(expr).id.asInstanceOf[R]
+        case Poke(signal, value) => signal.poke(value)
+        case Peek(signal) => signal.peek()
+        case Value(r) => r
+        case _ => ???
+      }
     }
   }
 }
--- src/test/scala/chiselverify/Primitives.scala
+++ src/test/scala/chiselverify/Primitives.scala
@@ -25,37 +25,36 @@ class PrimitivesSpec extends AnyFlatSpec with ChiselScalatestTester {
         _ <- step(1)
         v4 <- peek(c.a)
       } yield (v1.litValue, v2.litValue, v3.litValue, v4.litValue)
-      val vm = new VM(c.clock)
-      val result = vm.run(program)
+      val result = VM.run(program, c.clock)
       assert(result == (0, 10, 0, 1))
     }
   }
 
-  // "poke" should "drive the circuit IO" in {
-  //   class Pokable extends Module {
-  //     val a = IO(Input(UInt(32.W)))
-  //     val aOut = IO(Output(UInt(32.W)))
-  //     val aReg = RegNext(a) // pipelined loopback
-  //     aOut := aReg
-  //   }
+  "poke" should "drive the circuit IO" in {
+    class Pokable extends Module {
+      val a = IO(Input(UInt(32.W)))
+      val aOut = IO(Output(UInt(32.W)))
+      val aReg = RegNext(a) // pipelined loopback
+      aOut := aReg
+    }
 
-  //   def pokeOne(signal: UInt, value: UInt): Command[Unit] =
-  //     for {
-  //       _ <- poke(signal, value)
-  //       _ <- step(1)
-  //     } yield ()
+    def pokeOne(signal: UInt, value: UInt): Expr[Unit] =
+      for {
+        _ <- poke(signal, value)
+        _ <- step(1)
+      } yield ()
 
-  //   test(new Pokable()) { c =>
-  //     val program = for {
-  //       _ <- pokeOne(c.a, 100.U)
-  //       v1 <- peek(c.aOut)
-  //       _ <- step(100)
-  //       v2 <- peek(c.aOut)
-  //       _ <- pokeOne(c.a, 200.U)
-  //       v3 <- peek(c.aOut)
-  //     } yield (v1.litValue, v2.litValue, v3.litValue)
-  //     val result = Command.unsafeRun(program, c.clock, false)
-  //     assert(result.retval == (100, 100, 200))
-  //   }
-  // }
+    test(new Pokable()) { c =>
+      val program = for {
+        _ <- pokeOne(c.a, 100.U)
+        v1 <- peek(c.aOut)
+        _ <- step(100)
+        v2 <- peek(c.aOut)
+        _ <- pokeOne(c.a, 200.U)
+        v3 <- peek(c.aOut)
+      } yield (v1.litValue, v2.litValue, v3.litValue)
+      val result = VM.run(program, c.clock)
+      assert(result == (100, 100, 200))
+    }
+  }
 }
